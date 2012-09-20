bf7b0f2461687a7c43e71f340922b90f828b4296
Brian Reber <reber.brian@gmail.com>
2012-09-20 14:08:59 -0500

    Reorganize structure

---
+++ CokeRewards/CokeRewards/CokeRewardsRequest.h
@@ -0,0 +1,13 @@
+//
+//  CokeRewardsRequest.h
+//  CokeRewards
+//
+//  Created by Brian Reber on 9/20/12.
+//  Copyright (c) 2012 Brian Reber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CokeRewardsRequest : NSObject
+
+@end
---
+++ CokeRewards/CokeRewards/CokeRewardsRequest.m
@@ -0,0 +1,13 @@
+//
+//  CokeRewardsRequest.m
+//  CokeRewards
+//
+//  Created by Brian Reber on 9/20/12.
+//  Copyright (c) 2012 Brian Reber. All rights reserved.
+//
+
+#import "CokeRewardsRequest.h"
+
+@implementation CokeRewardsRequest
+
+@end
